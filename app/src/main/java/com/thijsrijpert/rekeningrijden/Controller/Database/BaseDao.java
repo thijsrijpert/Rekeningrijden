040d586ae0929ffd6d2daa196e101225b0d51040
Thijs Rijpert <60793766+thijsrijpert@users.noreply.github.com>
2020-03-19 11:52:51 +0100

    Fixed a potential error due to generics and varargs being used in the parameters

--- app/src/main/java/com/thijsrijpert/rekeningrijden/Controller/Database/BaseDao.java
+++ app/src/main/java/com/thijsrijpert/rekeningrijden/Controller/Database/BaseDao.java
@@ -12,12 +12,12 @@ public interface BaseDao<T> {
      * @param object the model objects that should be inserted into the database
      */
     @Insert
-    void insert(T... object);
+    void insert(T object);
 
     /**
      * Update a collection of model objects based on the primary key. The primary key cannot be updated
      * @param object the model objects that should be updated.
      */
     @Update
-    void update(T... object);
+    void update(T object);
 }
